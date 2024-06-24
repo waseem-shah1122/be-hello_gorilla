28b7bedac11f0ab4b4268c798d77784aa0bc05ed
WaseemShah <waseem.shah@rtechnologiespk.com>
2024-06-24 18:25:49 +0500

    first commit

--- src/main/java/com/rtechnologies/hello_gorilla/repository/MeetUpUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rtechnologies.hello_gorilla.repository;
-
-import com.rtechnologies.hello_gorilla.entity.MeetUpUserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetUpUserRepository extends JpaRepository<MeetUpUserEntity,Long> {
-}
